7131bd14904417fe6b9e03aeeded7046d84077c6
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-19 11:48:23 +0200

    Entity class created

---
+++ InheritanceBankAccount/InheritanceBankAccount.ClassLib/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InheritanceBankAccount.ClassLib
+{
+    public abstract class Entity
+    {
+        int id;
+
+        public Entity()
+        {
+
+        }
+        public Entity(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get => id; set => id = value; }
+    }
+}
